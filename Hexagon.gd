a5e0d1f01aa54b61456bff8d22d91f7d8535b3ff
Karsten Bock <KarstenBock@gmx.net>
2022-09-21 06:55:46 +0200

    Prepare for 4.0

---
+++ Hexagon.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+signal selected
+signal deselected
+signal cursor_entered
+signal cursor_exited
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_selected():
+	$Hexagon/Selected.visible = true
+
+func _on_deselected():
+	$Hexagon/Selected.visible = false
+	
+func _on_cursor_entered():
+	$Hexagon/Outline.default_color = Color.white
+	$Hexagon/Outline.z_index = 2
+	
+func _on_cursor_exited():
+	$Hexagon/Outline.default_color = Color.black
+	$Hexagon/Outline.z_index = 1
